c4237fd4e6d2f79528e3dd1c7efb422abe59c549
Tim Wade <hello@timjwade.com>
2017-12-02 15:29:32 -0800

    Use reagent

--- src/vcs3/core.cljs
+++ src/vcs3/core.cljs
@@ -1,5 +1,5 @@
 (ns vcs3.core
-    (:require ))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
@@ -10,8 +10,16 @@
 (defonce app-state (atom {:text "Hello world!"}))
 
 
+(defn hello-world []
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "Edit this and watch it change!"]])
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
