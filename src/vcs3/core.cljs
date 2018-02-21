b175ba18a215cdf688620ae8fb9fc4c18fcbe8b2
Tim Wade <hello@timjwade.com>
2018-02-21 12:14:27 -0800

    Add a range input that hooks up to osc1

--- src/vcs3/core.cljs
+++ src/vcs3/core.cljs
@@ -3,19 +3,40 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {}))
-
-(defn oscillator []
-  (let [context (new js/window.AudioContext)
-        oscillator (.createOscillator context)]
-    (set! (.-type oscillator) "square")
+(defn create-oscillator [context type]
+  (let [oscillator (.createOscillator context)]
+    (set! (.-type oscillator) type)
     (set! (.-value (.-frequency oscillator)) 440)
     (.connect oscillator (.-destination context))
-    (.start oscillator)))
+    (.start oscillator)
+    oscillator))
+
+(defonce context (new js/window.AudioContext))
+(defonce oscillator-1 (create-oscillator context "sine"))
+
+(defonce vcs3-data
+  (atom {:context context
+         :oscillator-1 {:frequency 440 :shape "sine"}}))
+
+(defn update-vcs3
+  []
+  (set! (.-value (.-frequency oscillator-1)) (:frequency (:oscillator-1 @vcs3-data))))
+
+(defn turny-dial
+  [component attribute value min max]
+  [:input {:type "range" :value value :min min :max max
+           :style {:width "100%"}
+           :on-change (fn [e]
+                        (swap! vcs3-data assoc-in [component attribute] (.. e -target -value))
+                        (update-vcs3))}])
 
 (defn vcs3 []
-  [:div {:on-click #(oscillator)}
-   [:img {:src "/images/vcs3.jpg" :alt "VCS3"}]])
+  [:div
+   [:h3 "Oscillator 1"]
+   [:p "Oscillator 1 is oscillating at " (:frequency (:oscillator-1 @vcs3-data)) " Hz."]
+   [turny-dial :oscillator-1 :frequency (:frequency (:oscillator-1 @vcs3-data)) 1 10000]
+   [:div
+    [:img {:src "/images/vcs3.jpg" :alt "VCS3"}]]])
 
 (reagent/render-component [vcs3]
                           (. js/document (getElementById "app")))
